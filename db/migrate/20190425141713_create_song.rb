d569f1731ce176df8c588acc3a25ae049d11d89e
Ivan Szebenszki <szib77@gmail.com>
2019-04-25 15:23:32 +0100

    Add Artist and Song

---
+++ db/migrate/20190425141713_create_song.rb
@@ -0,0 +1,11 @@
+class CreateSong < ActiveRecord::Migration[5.2]
+  def change
+    create_table :songs do |t|
+      t.string :title
+      t.references :artist
+    
+      t.timestamps
+    end
+    
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,10 @@
 # Add seed data here. Seed your database with `rake db:seed`
+Artist.destroy_all
+Song.destroy_all
 
+a1 = Artist.create(name: 'AC/DC')
+a2 = Artist.create(name: 'Metallica')
+
+s1 = Song.create(title: 'Song1', artist: a1)
+s2 = Song.create(title: 'Song2', artist: a2)
+s3 = Song.create(title: 'Song3', artist: a1)
