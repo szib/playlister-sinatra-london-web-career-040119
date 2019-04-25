8a6f4f7d976a52861879861fc642ad4222360730
Ivan Szebenszki <szib77@gmail.com>
2019-04-25 17:49:50 +0100

    Add seeding

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,16 +4,18 @@ Song.destroy_all
 Genre.destroy_all
 SongGenre.destroy_all
 
-a1 = Artist.create(name: 'AC/DC')
-a2 = Artist.create(name: 'Metallica')
+# a1 = Artist.create(name: 'AC/DC')
+# a2 = Artist.create(name: 'Metallica')
 
-s1 = Song.create(name: 'Song1', artist: a1)
-s2 = Song.create(name: 'Song2', artist: a2)
-s3 = Song.create(name: 'Song3', artist: a1)
+# s1 = Song.create(name: 'Song1', artist: a1)
+# s2 = Song.create(name: 'Song2', artist: a2)
+# s3 = Song.create(name: 'Song3', artist: a1)
 
-g1 = Genre.create(name: 'Pop')
-g2 = Genre.create(name: 'Jazz')
+# g1 = Genre.create(name: 'Pop')
+# g2 = Genre.create(name: 'Jazz')
 
-s1.genres << g1
-s1.genres << g2
-s2.genres << g1
+# s1.genres << g1
+# s1.genres << g2
+# s2.genres << g1
+
+LibraryParser.parse
